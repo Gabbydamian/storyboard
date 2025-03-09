934d40eeaa4eb5f4901d7a2b2c1706be58d02124
Damian Gabriel <gabbydamian92@gmail.com>
2025-03-09 17:50:37 +0100

    [Milestone]: Safe Restore Point before Server Actions

---
+++ src/actions/story.ts
@@ -0,0 +1,48 @@
+"use server";
+import { supabase } from "@/lib/supabse";
+import { Story } from "@/types/story";
+
+export async function getStories() {
+  const { data, error } = await supabase.from("stories").select();
+
+  if (error) {
+    console.error(error);
+    throw new Error(error.message);
+  }
+
+  return data || [];
+}
+
+export async function deleteStory(id: string) {
+  const { error } = await supabase.from("stories").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error(error.message);
+  }
+
+  // Fetch updated stories
+  return getStories();
+}
+
+export async function addStory(story: Partial<Story>) {
+  const { error } = await supabase.from("stories").insert([story]);
+
+  if (error) {
+    console.error(error);
+    throw new Error(error.message);
+  }
+
+  return getStories();
+}
+
+export async function updateStory(story: Partial<Story>, id: string) {
+  const { error } = await supabase.from("stories").update(story).eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error(error.message);
+  }
+
+  return getStories();
+}
